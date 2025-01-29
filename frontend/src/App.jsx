cf77181fe254dcc9b7cb814bb928084d5e8d0d38
Andrew Jovellanos <150148928+AndroBOI@users.noreply.github.com>
2025-01-29 13:50:14 +0800

    set up project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './assets/Styles/main.scss'
+import LoginPage from './Pages/LoginPage'
+function App() {
+  
+
+  return (
+    <>
+      <LoginPage/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/Components/LoginComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginComponent = () => {
+return (
+    <>
+        <input type="text" />
+    </>
+);
+};
+
+export default LoginComponent;
