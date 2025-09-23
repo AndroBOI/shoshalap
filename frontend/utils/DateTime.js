3acd787a901ba38749136ab9097416afcf25ba57
andrew <andrewjovellanos48@gmail.com>
2025-09-23 11:01:50 +0800

    reset

--- frontend/utils/DateTime.js
+++
@@ -1,7 +0,0 @@
-export function date(date = new Date()) {
-    const months = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    return `${months[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
-}
