09a71bd58c41b0266c08dfd405733069018044f7
andrew <andrewjovellanos48@gmail.com>
2025-10-08 14:41:11 +0800

    create comment action

--- actions/post.action.ts
+++ actions/post.action.ts
@@ -78,7 +78,6 @@ export const getPosts = async () => {
     }
 }
 
-
 export const toggleLike = async (postId: string) => {
     try {
         const userId = await getDbUserId()
@@ -136,6 +135,59 @@ export const toggleLike = async (postId: string) => {
         return {successs: true}
 
     } catch (error) {
+        console.error("Failed to toggle like:", error)
+        return {success: false, error: "Failed to toggle like"}
+    }
+}
 
+
+export const createComment = async (postId: string, content: string) => {
+    try {
+        const userId = await getDbUserId()
+
+        if(!userId) return
+        if(!content) throw new Error("Content is required")
+
+        
+
+        const post = await prisma.post.findUnique({
+            where: {id: postId},
+            select: {authorId: true}
+        })
+
+
+        if(!post) throw new Error("Post not found")
+
+
+        const [comment] = await prisma.$transaction(async (tx) => {
+            const newComment = await tx.comment.create({
+                data: {
+                    content,
+                    authorId: userId,
+                    postId
+                }
+            })
+
+            if(post.authorId !== userId) {
+                await tx.notification.create({
+                    data: {
+                        type: "COMMENT",
+                        userId: post.authorId,
+                        creatorId: userId,
+                        postId,
+                        commentId: newComment.id,
+                    }
+                })
+            }
+
+            return [newComment]
+
+        })
+        revalidatePath("/")
+        return {success: true, comment}
+
+    } catch (error) {
+        console.error("Failed to create comment:", error)
+        return {success: false, error: "Failed to create comment"}
     }
 }
